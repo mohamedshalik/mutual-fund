3e5620fe19f18158eab15b28f444225567bfca63
unknown <mohamedshalik@gmail.com>
2018-05-17 07:10:32 +0530

    changed the access orgini to port 3000

--- mutual-fund/app.js
+++ mutual-fund/app.js
@@ -59,5 +59,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen('3000', function(){
+  console.log('running on 3000...');
+});
 module.exports = app;
